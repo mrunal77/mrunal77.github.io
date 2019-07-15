b9dc0b15476721a2dbed74bfc8fcfd66d5de578c
mrunal77 <mtmeshram77@outlook.com>
2019-07-15 17:49:38 +0530

    new folder react bulded code

---
+++ new/precache-manifest.ea61a6a4cb9c725c4f656996357fe4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64edb4932f7732cbed2efc8e31d75222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4e720c84d7f9369370",
+    "url": "/static/css/main.9513dbf3.chunk.css"
+  },
+  {
+    "revision": "8d5dbd4b9e58488ed3ee",
+    "url": "/static/js/2.1b248f81.chunk.js"
+  },
+  {
+    "revision": "2e4e720c84d7f9369370",
+    "url": "/static/js/main.725fbe50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01d9554636e9b427d35b274aa3b8f0e8",
+    "url": "/static/media/bannerImage.01d95546.jpg"
+  }
+]);
