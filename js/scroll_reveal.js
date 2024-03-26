340c621ce92e2eeaed87503828e039c5b69b2e5f
mrunal77 <mtmeshram77@outlook.com>
2024-03-26 19:36:28 +0530

    Scroll Reveal Code Seperation

---
+++ js/scroll_reveal.js
@@ -0,0 +1,72 @@
+document.addEventListener("contextmenu", event => event.preventDefault());
+    window.sr = ScrollReveal({
+      reset: true
+    });
+    sr.reveal(".banner-center", {
+      duration: 3000,
+      origin: "top"
+    });
+    sr.reveal(".pad1", {
+      duration: 2000,
+      origin: "bottom"
+    });
+    sr.reveal(".cSharp", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".cSharp", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".Sql", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".Mvc", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".skillTitle", {
+      duration: 2000,
+      origin: "left"
+    });
+    sr.reveal(".engiNeering", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".twelth", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".tenth", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".certificate", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".diploma", {
+      duration: 2000,
+      origin: "right"
+    });
+    sr.reveal(".educationTitle", {
+      duration: 2000,
+      origin: "left"
+    });
+    sr.reveal(".projects", {
+      duration: 2000,
+      origin: "bottom"
+    });
+    sr.reveal(".contact", {
+      duration: 2000,
+      origin: "bottom"
+    });
+    sr.reveal(".contact-title", {
+      duration: 2000,
+      origin: "left"
+    });
+    sr.reveal(".icons", {
+      duration: 2000,
+      origin: "bottom"
+    });
